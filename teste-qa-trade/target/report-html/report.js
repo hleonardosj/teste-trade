644cb55bca973ea5f691b58dc3da1b9af824e9c9
HT Series <HT Series@DESKTOP-JR4DN9G.local>
2021-01-21 21:31:56 -0300

    Adicionando os relatórios nos formatos html e json

---
+++ teste-qa-trade/target/report-html/report.js
@@ -0,0 +1,739 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("AutomatizarRotina.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "# language: pt"
+    }
+  ],
+  "line": 3,
+  "name": "como QA da Trade Technology eu quero automatizar a rotina de adiconar produtos",
+  "description": "",
+  "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos",
+  "keyword": "Funcionalidade",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@AutomatizarRotina"
+    },
+    {
+      "line": 2,
+      "name": "@Wip"
+    }
+  ]
+});
+formatter.scenarioOutline({
+  "line": 6,
+  "name": "Testar todos os fluxos da loja ate chegar em payment",
+  "description": "",
+  "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos;testar-todos-os-fluxos-da-loja-ate-chegar-em-payment",
+  "type": "scenario_outline",
+  "keyword": "Esquema do Cenario"
+});
+formatter.step({
+  "line": 7,
+  "name": "que eu acesso o site automationpractice",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 8,
+  "name": "eu clicar na opcao best sellers",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 9,
+  "name": "clicar em Add to cart em um dos produtos",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 10,
+  "name": "o produto e adicionado com sucesso",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 11,
+  "name": "eu faco isso com todos os outros produtos",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 12,
+  "name": "eu exporto os dados do summary em um arquivo em formato json",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 13,
+  "name": "que eu clico em Cart e Proceed To Checkout",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 14,
+  "name": "informo os dados de email para criar uma conta",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 15,
+  "name": "clico em create an account",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 16,
+  "name": "o formulario de criacao de conta e exibido",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 17,
+  "name": "eu preencho os campos obrigatorios do formulario",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 18,
+  "name": "eu clico em Register",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 19,
+  "name": "Valido se foi para a etapa endereco",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 20,
+  "name": "eu informo os dados de \"\u003cemail\u003e\" e \"\u003csenha\u003e\"",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 21,
+  "name": "eu clicar em Sign in",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 22,
+  "name": "a pagina de cadastro de endereco e exibida",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 23,
+  "name": "que eu clico em Proceed To Checkout",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 24,
+  "name": "a pagina de envio e exibida",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 25,
+  "name": "que eu marco os termos de servico",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 26,
+  "name": "eu clicar em Proceed To Checkout",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 27,
+  "name": "a pagina de pagamento e exibida",
+  "keyword": "Entao "
+});
+formatter.examples({
+  "line": 29,
+  "name": "",
+  "description": "",
+  "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos;testar-todos-os-fluxos-da-loja-ate-chegar-em-payment;",
+  "rows": [
+    {
+      "cells": [
+        "email",
+        "senha"
+      ],
+      "line": 30,
+      "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos;testar-todos-os-fluxos-da-loja-ate-chegar-em-payment;;1"
+    },
+    {
+      "cells": [
+        "hleonardosj+test@gmail.com",
+        "12345"
+      ],
+      "line": 31,
+      "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos;testar-todos-os-fluxos-da-loja-ate-chegar-em-payment;;2"
+    },
+    {
+      "cells": [
+        "hleonardosj+test1@gmail.com",
+        "56789"
+      ],
+      "line": 32,
+      "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos;testar-todos-os-fluxos-da-loja-ate-chegar-em-payment;;3"
+    }
+  ],
+  "keyword": "Exemplos"
+});
+formatter.scenario({
+  "line": 31,
+  "name": "Testar todos os fluxos da loja ate chegar em payment",
+  "description": "",
+  "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos;testar-todos-os-fluxos-da-loja-ate-chegar-em-payment;;2",
+  "type": "scenario",
+  "keyword": "Esquema do Cenario",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@Wip"
+    },
+    {
+      "line": 2,
+      "name": "@AutomatizarRotina"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "que eu acesso o site automationpractice",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 8,
+  "name": "eu clicar na opcao best sellers",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 9,
+  "name": "clicar em Add to cart em um dos produtos",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 10,
+  "name": "o produto e adicionado com sucesso",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 11,
+  "name": "eu faco isso com todos os outros produtos",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 12,
+  "name": "eu exporto os dados do summary em um arquivo em formato json",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 13,
+  "name": "que eu clico em Cart e Proceed To Checkout",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 14,
+  "name": "informo os dados de email para criar uma conta",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 15,
+  "name": "clico em create an account",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 16,
+  "name": "o formulario de criacao de conta e exibido",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 17,
+  "name": "eu preencho os campos obrigatorios do formulario",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 18,
+  "name": "eu clico em Register",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 19,
+  "name": "Valido se foi para a etapa endereco",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 20,
+  "name": "eu informo os dados de \"hleonardosj+test@gmail.com\" e \"12345\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 21,
+  "name": "eu clicar em Sign in",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 22,
+  "name": "a pagina de cadastro de endereco e exibida",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 23,
+  "name": "que eu clico em Proceed To Checkout",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 24,
+  "name": "a pagina de envio e exibida",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 25,
+  "name": "que eu marco os termos de servico",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 26,
+  "name": "eu clicar em Proceed To Checkout",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 27,
+  "name": "a pagina de pagamento e exibida",
+  "keyword": "Entao "
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuAcessoOSiteAutomationpractice()"
+});
+formatter.result({
+  "duration": 12469055900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicarNaOpcaoBestSellers()"
+});
+formatter.result({
+  "duration": 115801300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.clicarEmAddToCardEmUmDosProdutos()"
+});
+formatter.result({
+  "duration": 2749464900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.oProdutoEAdicionadoComSucesso()"
+});
+formatter.result({
+  "duration": 53678600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euFacoIssoComTodosOsOutrosProdutos()"
+});
+formatter.result({
+  "duration": 34428276100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euExportoOsDadosDoSummaryEmUmArquivoEmFormatoJson()"
+});
+formatter.result({
+  "duration": 24600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuClicoEmCartEProceedToCheckout()"
+});
+formatter.result({
+  "duration": 5503130300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.informoOsDadosDeEmailParaCriarUmaConta()"
+});
+formatter.result({
+  "duration": 41400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.clicoEmCreateAnAccount()"
+});
+formatter.result({
+  "duration": 23400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.oFormularioDeCriacaoDeContaEExibido()"
+});
+formatter.result({
+  "duration": 21100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euPreenchoOsCamposObrigatoriosDoFormulario()"
+});
+formatter.result({
+  "duration": 44100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicoEmRegister()"
+});
+formatter.result({
+  "duration": 21700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.validoSeFoiParaAEtapaEndereco()"
+});
+formatter.result({
+  "duration": 25900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "hleonardosj+test@gmail.com",
+      "offset": 24
+    },
+    {
+      "val": "12345",
+      "offset": 55
+    }
+  ],
+  "location": "AutomatizarRotinas.euInformoOsDadosDeE(String,String)"
+});
+formatter.result({
+  "duration": 201769500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicarEmSignIn()"
+});
+formatter.result({
+  "duration": 2120688200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.aPaginaDeCadastroDeEnderecoEExibida()"
+});
+formatter.result({
+  "duration": 55121500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuClicoEmProceedToCheckout()"
+});
+formatter.result({
+  "duration": 1875204700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.aPaginaDeEnvioEExibida()"
+});
+formatter.result({
+  "duration": 54262200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuMarcoOsTermosDeServico()"
+});
+formatter.result({
+  "duration": 71861500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicarEmProceedToCheckout()"
+});
+formatter.result({
+  "duration": 1676911100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.aPaginaDePagamentoEExibida()"
+});
+formatter.result({
+  "duration": 20400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 32,
+  "name": "Testar todos os fluxos da loja ate chegar em payment",
+  "description": "",
+  "id": "como-qa-da-trade-technology-eu-quero-automatizar-a-rotina-de-adiconar-produtos;testar-todos-os-fluxos-da-loja-ate-chegar-em-payment;;3",
+  "type": "scenario",
+  "keyword": "Esquema do Cenario",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@Wip"
+    },
+    {
+      "line": 2,
+      "name": "@AutomatizarRotina"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "que eu acesso o site automationpractice",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 8,
+  "name": "eu clicar na opcao best sellers",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 9,
+  "name": "clicar em Add to cart em um dos produtos",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 10,
+  "name": "o produto e adicionado com sucesso",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 11,
+  "name": "eu faco isso com todos os outros produtos",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 12,
+  "name": "eu exporto os dados do summary em um arquivo em formato json",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 13,
+  "name": "que eu clico em Cart e Proceed To Checkout",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 14,
+  "name": "informo os dados de email para criar uma conta",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 15,
+  "name": "clico em create an account",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 16,
+  "name": "o formulario de criacao de conta e exibido",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 17,
+  "name": "eu preencho os campos obrigatorios do formulario",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 18,
+  "name": "eu clico em Register",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 19,
+  "name": "Valido se foi para a etapa endereco",
+  "keyword": "E "
+});
+formatter.step({
+  "line": 20,
+  "name": "eu informo os dados de \"hleonardosj+test1@gmail.com\" e \"56789\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 21,
+  "name": "eu clicar em Sign in",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 22,
+  "name": "a pagina de cadastro de endereco e exibida",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 23,
+  "name": "que eu clico em Proceed To Checkout",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 24,
+  "name": "a pagina de envio e exibida",
+  "keyword": "Entao "
+});
+formatter.step({
+  "line": 25,
+  "name": "que eu marco os termos de servico",
+  "keyword": "Dado "
+});
+formatter.step({
+  "line": 26,
+  "name": "eu clicar em Proceed To Checkout",
+  "keyword": "Quando "
+});
+formatter.step({
+  "line": 27,
+  "name": "a pagina de pagamento e exibida",
+  "keyword": "Entao "
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuAcessoOSiteAutomationpractice()"
+});
+formatter.result({
+  "duration": 11155367500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicarNaOpcaoBestSellers()"
+});
+formatter.result({
+  "duration": 89958400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.clicarEmAddToCardEmUmDosProdutos()"
+});
+formatter.result({
+  "duration": 2916867000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.oProdutoEAdicionadoComSucesso()"
+});
+formatter.result({
+  "duration": 55124900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euFacoIssoComTodosOsOutrosProdutos()"
+});
+formatter.result({
+  "duration": 32479870300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euExportoOsDadosDoSummaryEmUmArquivoEmFormatoJson()"
+});
+formatter.result({
+  "duration": 16000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuClicoEmCartEProceedToCheckout()"
+});
+formatter.result({
+  "duration": 5624271400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.informoOsDadosDeEmailParaCriarUmaConta()"
+});
+formatter.result({
+  "duration": 14900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.clicoEmCreateAnAccount()"
+});
+formatter.result({
+  "duration": 10600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.oFormularioDeCriacaoDeContaEExibido()"
+});
+formatter.result({
+  "duration": 14100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euPreenchoOsCamposObrigatoriosDoFormulario()"
+});
+formatter.result({
+  "duration": 16500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicoEmRegister()"
+});
+formatter.result({
+  "duration": 10900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.validoSeFoiParaAEtapaEndereco()"
+});
+formatter.result({
+  "duration": 13400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "hleonardosj+test1@gmail.com",
+      "offset": 24
+    },
+    {
+      "val": "56789",
+      "offset": 56
+    }
+  ],
+  "location": "AutomatizarRotinas.euInformoOsDadosDeE(String,String)"
+});
+formatter.result({
+  "duration": 214219000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicarEmSignIn()"
+});
+formatter.result({
+  "duration": 1908470600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.aPaginaDeCadastroDeEnderecoEExibida()"
+});
+formatter.result({
+  "duration": 39330400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuClicoEmProceedToCheckout()"
+});
+formatter.result({
+  "duration": 1447469400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.aPaginaDeEnvioEExibida()"
+});
+formatter.result({
+  "duration": 35553100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.queEuMarcoOsTermosDeServico()"
+});
+formatter.result({
+  "duration": 78661200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.euClicarEmProceedToCheckout()"
+});
+formatter.result({
+  "duration": 1641173000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "AutomatizarRotinas.aPaginaDePagamentoEExibida()"
+});
+formatter.result({
+  "duration": 23500,
+  "status": "passed"
+});
+});
